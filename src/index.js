c986b3101575b6500399b06157fbd812a6e6ae17
Maxim Filimonov <tpaktopsp@gmail.com>
2022-05-24 20:29:31 +0300

    первый шаг в отдельный файл

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Step1 from "./Step1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Step1 />
   </React.StrictMode>
 );
