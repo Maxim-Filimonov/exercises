e36e58e3de72a3740f93919bc209c9cf9263e782
Maxim Filimonov <tpaktopsp@gmail.com>
2022-05-24 19:58:52 +0300

    1.2 класс компонент вместо хуков и заглушка для практики

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
 function App() {
   const minutes = new Date().getMinutes();
   const isEven = minutes % 2 === 0;
-  return <MinuteDisplay isEven={isEven} />;
+
+  return (
+    <>
+      <MinuteDisplay isEven={isEven} />
+      <FingerCounter />
+    </>
+  );
 }
 
 function MinuteDisplay(props) {
@@ -9,6 +16,36 @@ function MinuteDisplay(props) {
     return <h1>Четная минута</h1>;
   } else {
     return <h1>Нечетная минута</h1>;
+  }
+}
+
+class FingerCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handleIncrement() {
+    this.setState(({ count }) => ({
+      count: count + 1,
+    }));
+  }
+
+  render() {
+    let counter;
+    if (this.state.count <= 10) {
+      counter = <span>Можно посчитать по пальцам до {this.state.count}</span>;
+    } else {
+      counter = null;
+    }
+    return (
+      <>
+        <h1>Счетчик</h1>
+        <p>{counter}</p>
+        {/* ЗДЕСЬ нужно отобразить кнопку сбросить счетчик ТОЛЬКО когда счетчик больше 10 */}
+        <button onClick={() => this.handleIncrement()}>Добавить</button>
+      </>
+    );
   }
 }
 
