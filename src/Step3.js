c9a980cb8b0b0a93d57995f6a5f82db01f4abfc4
Maxim Filimonov <tpaktopsp@gmail.com>
2022-05-25 13:37:01 +0300

    1.2 используем ref для получения значения

--- src/Step3.js
+++ src/Step3.js
@@ -1,34 +1,54 @@
 import React from "react";
 import "./Step3.css";
 
-function App() {
-  return (
-    <form>
-      <fieldset>
-        <legend>Форма для создания курса</legend>
-        <div className="field">
-          <label for="courseName">Название курса</label>
-          <input id="courseName" type="text"></input>
-        </div>
-        <div className="field">
-          <label for="courseDescription">Описание курса</label>
-          <textarea id="courseDescription" />
-        </div>
-        <div className="field">
-          <label for="courseType">Тип курса</label>
-          <select>
-            <option value="online">Онлайн</option>
-            <option value="offline">Оффлайн</option>
-            <option value="hybrid">Гибрид</option>
-          </select>
-        </div>
-        <div className="field">
-          <label for="courseLogo">Логотип</label>
-          <input type="file" />
-        </div>
-      </fieldset>
-    </form>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.courseName = React.createRef();
+    this.courseDesc = React.createRef();
+    this.courseType = React.createRef();
+    this.courseLogo = React.createRef();
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(
+      this.courseName.current.value,
+      this.courseDesc.current.value,
+      this.courseType.current.value,
+      this.courseLogo.current.files[0].name
+    );
+  }
+
+  render() {
+    return (
+      <form onSubmit={(e) => this.handleSubmit(e)}>
+        <fieldset>
+          <legend>Форма для создания курса</legend>
+          <div className="field">
+            <label htmlFor="courseName">Название курса</label>
+            <input ref={this.courseName} id="courseName" type="text"></input>
+          </div>
+          <div className="field">
+            <label htmlFor="courseDescription">Описание курса</label>
+            <textarea ref={this.courseDesc} id="courseDescription" />
+          </div>
+          <div className="field">
+            <label htmlFor="courseType">Тип курса</label>
+            <select ref={this.courseType}>
+              <option value="online">Онлайн</option>
+              <option value="offline">Оффлайн</option>
+              <option value="hybrid">Гибрид</option>
+            </select>
+          </div>
+          <div className="field">
+            <label htmlFor="courseLogo">Логотип</label>
+            <input type="file" ref={this.courseLogo} />
+          </div>
+        </fieldset>
+        <input type="submit" value="Сохранить" />
+      </form>
+    );
+  }
 }
 
 export default App;
